ece8a27347fecc7ac32f8520b6da390b71abcbee
Saumtitri Behera <Saumtitri Behera@192.168.0.58>
2022-07-21 16:03:33 +0530

    saum commit

---
+++ src/PrograminYesICanDo/check.java
@@ -0,0 +1,16 @@
+package PrograminYesICanDo;
+
+public class check {
+
+	public static void main(String[] args) throws ArithmeticException {
+
+	try {
+		System.out.println(10/0);
+	}
+	catch(ArithmeticException e) {
+		System.out.println("handled");
+	}
+	System.out.println("shoukd ex");
+	}
+
+}
